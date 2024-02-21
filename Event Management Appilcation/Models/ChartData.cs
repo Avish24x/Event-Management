27c81895167c294629b278bc3164a41d0a0510c5
Yuvi002 <gangaramyogesh@gmail.com>
2024-02-21 09:22:28 +0400

    Deleting unnecessary list of models

--- Event Management Appilcation/Models/ChartData.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Event_Management_Appilcation.Models
-{
-    public class ChartData
-    {
-        public string Label { get; set; }
-        public int Value { get; set; }
-    }
-}
--- Event Management Appilcation/Models/Response.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Event_Management_Appilcation.Models
-{
-    public class Response
-        
-    {
-        public string? Status {  get; set; }
-        public string? Message { get; set;}
-    }
-}
