4be3fb00c65d43590ca504d8f2267b074b548eb6
Yuvi002 <gangaramyogesh@gmail.com>
2024-02-25 13:45:03 +0400

    Pushing new Data

--- Event.Management.Data/Migrations/20240225064358_SecondMig.Designer.cs
+++ Event.Management.Data/Migrations/20240225064358_SecondMig.Designer.cs
@@ -4,6 +4,7 @@ using Event_Managemenent.Data.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Event.Management.Data.Migrations
 {
     [DbContext(typeof(ApplicationUser))]
-    partial class ApplicationUserModelSnapshot : ModelSnapshot
+    [Migration("20240225064358_SecondMig")]
+    partial class SecondMig
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
